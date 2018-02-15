7f98c5dc39b9f5fe7d6ac988c9e9c7e3cce9d258
andriy <andriystasenko22@gmail.com>
2018-02-15 16:21:17 +0200

    same

--- build/js/comon.js
+++ build/js/comon.js
@@ -1,26 +1,68 @@
 $(document).ready(function () {
 
-
-    // var location =window.location.pathname.toString();
-    // if( location.charAt( 0 ) === '/' ) {
-    //     location = location.slice(1);
-    //     // console.log(location);
-    // }
-
-    $(document).on('click', '.quick-list a', function (e) {
-       e.preventDefault();
-       var id_element = $(this).attr('href');
-       var scroll_element = $(id_element);
-       $('html, body').animate({'scrollTop':scroll_element.offset().top},700);
+    // Активний пункт навігації на відповідній сторінці
+    $('.navigation-list a').each(function () {
+        if ($(this).attr('href') == location.href.split("/").slice(-1)) {
+            $(this).parent('li').addClass('active');
+        }
     });
 
-    $('.navigation-list .navigation-item').each(function () {
-       if($(this).attr('href')===location){
-           $(this).addClass('active');
-       }
+    // слайдер перемикання сторінок
+    $('.navigation-button-up').on('click', function (e) {
+        e.preventDefault();
+        var $active = $('.navigation-list li.active');
+        var url;
+        if ($active.length == 0) {
+            $active = $('.navigation-list li:last');
+            url = $active.children('a').attr('href');
+        }
+        else {
+            if ($active.prev('li').length > 0) {
+                url = $active.prev('li').children('a').attr('href');
+            } else {
+                url = $('.navigation-list li:last a').attr('href');
+            }
+        }
+        window.location = url;
     });
 
-    if($('.about-slider').length>0) {
+    $('.navigation-button-down').on('click', function (e) {
+        e.preventDefault();
+        var $active = $('.navigation-list li.active');
+        var url;
+        if ($active.length == 0) {
+            $active = $('.navigation-list li:first');
+            url = $active.children('a').attr('href');
+        }
+        else {
+            if ($active.next('li').length > 0) {
+                url = $active.next('li').children('a').attr('href');
+            } else {
+                url = $('.navigation-list li:first a').attr('href');
+            }
+        }
+        window.location = url;
+    });
+
+    $(document).on('click', '.home-layout .menu-block .menu-list a, .home-layout .quick-list a', function (e) {
+        e.preventDefault();
+        var id_element = $(this).attr('href');
+        var scroll_element = $(id_element);
+        $('html, body').animate({'scrollTop': scroll_element.offset().top}, 700);
+        if ($(this).closest('ul').hasClass('menu-list')) {
+            $('.menu-block').animate({'left': '-100%'}, 500, function () {
+                $('.menu-visible-block').animate({'left': '0%'}, 500);
+            });
+        }
+    });
+
+    // $('.navigation-list .navigation-item').each(function () {
+    //     if ($(this).attr('href') === location) {
+    //         $(this).addClass('active');
+    //     }
+    // });
+
+    if ($('.about-slider').length > 0) {
         $('.about-slider').slick({
             centerMode: true,
             infinite: true,
@@ -94,4 +136,20 @@ $(document).ready(function () {
         $('html, body').animate({'scrollTop': '0'}, 700);
     });
 
+
+
+    $('.slider-text-button').on('click', function (e) {
+        e.preventDefault();
+        var $active = $('.slider-text-content .slider-text-info.active');
+        if($active.length == 0) {
+            $active = $('.slider-text-content .slider-text-info:first');
+        }
+        $active.removeClass('active');
+        if($active.next('.slider-text-info').length > 0) {
+            $active.next('.slider-text-info').addClass('active');
+        } else {
+            $('.slider-text-content .slider-text-info:first').addClass('active');
+        }
+
+    });
 });
